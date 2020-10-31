299a7a2c3a1fa5ebd8438e8dbf909bf700d0f28c
792413886 <73692288+792413886@users.noreply.github.com>
2020-10-31 21:04:08 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   //Stellen Sie zwei Variablen ein
+    int num1=0;
+    int num2=0;
+    int Sum=0;
+    //Addition
+    scanf("%d%d",&num1,&num2);
+    Sum=num1+num2;
+    printf("Sum=%d\n",Sum);
+    return 0;
+}
